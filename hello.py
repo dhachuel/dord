8d5c18da80c505ef21039156ded4e20bd8286781
dhachuel <dhachuel@gmail.com>
2014-09-12 07:03:03 -0400

    second commit, first html

--- hello.py
+++ hello.py
@@ -1,12 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, abort, redirect, url_for, request, session, escape, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello world!"
+def index():
+    return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
